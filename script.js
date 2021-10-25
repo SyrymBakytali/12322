704b776ade23780afb3af40b9aba9268ad1acdf0
SyrymBakytali <78458946+SyrymBakytali@users.noreply.github.com>
2021-10-25 13:17:35 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function multiplyBy()
+{
+        num1 = document.getElementById("bachelor").value = 1.5;
+        num2 = document.getElementById("college").value = 1.2;
+        num3 = document.getElementById("high_schoo").value = 1.05;
+        num4 = document.getElementById("middle_schoo").value = 0.9;
+        document.getElementById("result").innerHTML = num1 * 100;
+}
